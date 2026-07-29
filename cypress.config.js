aed25a15383b20dbf5644b1a427351a36946e2b4
Lucas Santos <dosantosbjj@gmail.com>
2026-07-28 21:27:11 -0300

    ci: add Github Actions workflow for headless Cypress + mochawesome and update gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,14 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   allowCypressEnv: true,
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    reportFilename: "results",
+    overwrite: false,
+    html: true,
+    json: true,
+  },
   e2e: {
     baseUrl: "https://front.serverest.dev/login/",
     env: {
